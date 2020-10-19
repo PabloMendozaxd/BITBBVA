eb667ba6dfacf2116406e53e44a8ab37a10c54b8
PabloMendozaxd <jose.mendozaji@my.unitec.edu.mx>
2020-10-19 12:42:10 -0500

    correción en configuración de puerto

--- config.js
+++ config.js
@@ -1,13 +1,5 @@
-const express = require('express');
-
-const config = require('../config.js');
-const user = require('./components/user/network');
-
-const app = express();
-
-// ROUER
-app.use('/api/user', user);
-
-app.listen(config.api.port, () => {
-    console.log('Api escuchando en el puerto ', config.api.port);
-});
+module.exports = {
+    api: {
+        port: process.env.API_PORT || 3000,
+    }
+}
