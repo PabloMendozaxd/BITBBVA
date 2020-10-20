79c7de45b739cdf52bb59a9cebc7912c8da35cf0
PabloMendozaxd <jose.mendozaji@my.unitec.edu.mx>
2020-10-19 19:57:31 -0500

    Get y list funcionando

--- config.js
+++ config.js
@@ -1,5 +1,14 @@
-module.exports = {
-    api: {
-        port: process.env.API_PORT || 3000,
+module.exports={
+    api:{
+        port:process.env.API_PORT||3000,
+    },
+    jwt:{
+        secret: process.env.JWT_SECRET||'notasecret',
+    }, 
+    mysql:{
+        host:process.env.MYSQL_HOST || 'remotemysql.com',
+        user:process.env.MYSQL_USER || 'hIi38Rdntw',
+        password:process.env.MYSQL_PASS || 'dbBBtGs6FU',
+        database:process.env.MYSQL_DB || 'hIi38Rdntw',
     }
 }
--- store/pruebita.js
+++ store/pruebita.js
@@ -1,19 +1,40 @@
 const db = {
     'user': [
-        { id: 1, name: 'Pablo' },
+        { id: '1', name: 'Carlos' },
     ],
 };
 
-function list(tabla) {
-    return db[tabla];
+async function list(tabla) {
+    return db[tabla] || [];
 }
 
-function get(tabla, id) {
-    let col = list(tabla);
+async function get(tabla, id) {
+    let col = await list(tabla);
     return col.filter(item => item.id === id)[0] || null;
+}
+
+async function upsert(tabla, data) {
+    if (!db[tabla]) {
+        db[tabla] = [];
+    }
+
+    db[tabla].push(data);
+
+    console.log(db);
+}
+
+
+async function query(tabla, q) {
+    let col = await list(tabla);
+    let keys = Object.keys(q);
+    let key = keys[0];
+    
+    return col.filter(item => item[key] === q[key])[0] || null;
 }
 
 module.exports = {
     list,
     get,
+    upsert,
+    query,
 };
